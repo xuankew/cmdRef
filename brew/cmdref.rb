c41a007431eb54ef34d25a4a7d895b3849a451d0
xuanke <zhouliwei@mxbc.com>
2026-08-25 17:16:13 +0800

    release v0.1.0 - update SHA256

--- brew/cmdref.rb
+++ brew/cmdref.rb
@@ -6,21 +6,21 @@ class Cmdref < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0/cmdref-macos-aarch64"
-      sha256 "SHA256_PLACEHOLDER_MACOS_AARCH64"
+      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0"
+      sha256 "11954f93b215aa97d62a012da23c3c0be7fe4404fac82ed9d8ac09e8eb1d951b"
     else
-      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0/cmdref-macos-x86_64"
-      sha256 "SHA256_PLACEHOLDER_MACOS_X86_64"
+      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0"
+      sha256 "2be0bb707ce3aaafb05df6bd03a5471d8f1896cd2a96547178724717d0732676"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0/cmdref-linux-aarch64"
-      sha256 "SHA256_PLACEHOLDER_LINUX_AARCH64"
+      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0"
+      sha256 "9275ad5b5ccae0f9ea3a000b0a2c4629e9d3a8201f160cb983837577d5ca980d"
     else
-      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0/cmdref-linux-x86_64"
-      sha256 "SHA256_PLACEHOLDER_LINUX_X86_64"
+      url "https://github.com/xuankew/cmdRef/releases/download/v0.1.0"
+      sha256 "ab9f0b69b63be51188d433fb4c797f331674b757b05a30655e6e384ac9e0913e"
     end
   end
 
